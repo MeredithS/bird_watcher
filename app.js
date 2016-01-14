9c94f819a22afd8e05a4b9f8ea39d8d8e2be65ad
Meredith Salguero <salguero.meredith@gmail.com>
2016-01-14 14:41:39 -0500

    homework week13/day4

--- app.js
+++ app.js
@@ -1,15 +1,87 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/birds';
+var ObjectID = require('mongodb').ObjectID;
+var geocoder = require('geocoder');
+
+var db
 
 // Configuration
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs')
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
+MongoClient.connect(mongoUrl, function(err,database){
+	if(err){
+		console.log(err);
+	}
+	console.log('connected');
+	db = database;
+	process.on("exit", db.close);
+});
 
 // Routes
 app.get('/', function(req, res){
-  res.render('index');
+	db.collection('sightings').find({}).limit(3).sort({date:-1}).toArray(function(err,results){
+		results.forEach(function(result){
+			result.date = new Date(result.date)
+		})
+	res.render('index', {birds: results});	
+	})
+})  
+
+app.get('/sightings/new', function(req,res){
+	res.render('form');
+})
+
+app.get('/api/sightings',function(req,res){
+	db.collection('sightings').find({}).toArray(function(err,results){
+		res.json(results);
+	})
+})
+
+app.post('/sightings',function(req,res){
+	geocoder.geocode(req.body.sighting.location, function(err,result){
+		var latLong = result.results[0].geometry.location;
+			var objToSave = {
+				date: Date(), 
+				bird: req.body.sighting.bird, 
+				Location: req.body.sighting.location,
+				Lat_Long:latLong
+			}; 
+			db.collection('sightings').insert(objToSave, function(err,result){
+					 if(req.body['non-demo-form']){
+					 	res.redirect('/');
+					 }else{
+						res.send(objToSave);
+					}
+				})
+			})
 });
+
+app.get('/demo', function(req,res){
+	res.render('demo');
+})
+
+app.get('/sightings', function(req,res){
+	db.collection('sightings').find({}).toArray
+	(function(err,result){
+		res.json(result);
+	})
+})
+
+app.get('/sightings/:bird_id/edit', function(req,res){
+	console.log(req.params)
+	db.collection('sightings').findOne({ _id: ObjectID(req.params.bird_id)},function(err, result){
+		res.json(result);
+	})
+})
+
+app.put('/sightings/edit', function(req,res){
+	console.log(req.body);
+})
 
 app.listen(process.env.PORT || 3000);
--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,93 @@ console.log("        <')_,/ ")
 console.log("        (_==/  ")
 console.log("birder  ='-    ")
 console.log("we're hiring!  ")
+
+var getEditBird = function(){
+	console.log(this)
+	$.ajax({
+		url: '/sightings/'+$('#id').val()+'/edit',
+		type: 'GET',
+		dataType: 'json'
+	}).done(getEditBirdForm)
+}
+
+var getEditBirdForm = function(birdToEdit){
+	var source = $('#edit-bird-form').html();
+	var template = Handlebars.compile(source);
+	$('body').append(template(birdToEdit));
+	$('#update-bird').on('click', updateBird);
+}
+
+var updateBird = function(){
+	$.ajax({
+		url:'/sightings/edit',
+		type: "PUT",
+		dataType: 'json',
+		data:{
+			bird: $('#edit-bird-type').val(),
+			Location: $('#edit-location').val(),
+			id: $('#_id').val()
+		}
+	}).done();
+}
+
+var getBirds = function(){
+	$.ajax({
+		url: '/sightings',
+		type: 'GET',
+		dataType: 'json'
+	}).done(showBirds)
+};
+var showBirds = function(birds){
+	var source = $('#bird-list').html();
+	var template = Handlebars.compile(source);
+	birds.forEach(function(bird){
+		$('body').append(template(bird));
+	});
+	$('#edit-bird').on('click', getEditBird)
+};
+
+var addNewBird = function(){
+	$.ajax({
+		url: '/sightings',
+		type: 'GET',
+		dataType: 'json'
+	}).done(getBirdForm)
+};
+
+var getBirdForm = function(form){
+	var source = $('#add-sighting').html();
+	var template = Handlebars.compile(source);
+	$('body').append(template(form));
+	$('#add-sighting-btn').on('click', addBird);
+}
+
+var addBird = function(event){
+	event.preventDefault();
+	$.ajax({
+		url: '/sightings',
+		type: 'POST',
+		dataType: 'json',
+		data:{
+			sighting:{
+				location: $('#location').val(),
+				bird: $('#bird-type').val()
+			}		
+		}
+	}).done(function(data) {
+		var source = $('#bird-list').html();
+		var template = Handlebars.compile(source);
+		$('body').append(template(data));
+		$('#bird-type').val('');
+		$('#location').val('')
+	})
+}
+
+
+
+$(document).ready(function(){
+	$('#birds-link').on('click', getBirds);
+	$('#add-bird-link').on('click', addNewBird);
+
+
+	});
